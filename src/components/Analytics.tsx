97f0c8cb909e3d1d7cda6732d3d460b9c870d273
Austin Chick <auschick.work@gmail.com>
2025-03-09 03:31:28 -0700

    Add GTM to web vitals

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import WebVitals from "@/components/WebVitals";
-import { GoogleAnalytics } from "@next/third-parties/google";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 const Analytics = () => {
   return (
     <>
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID as string} />
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID as string} />
       <WebVitals />
     </>
   );
