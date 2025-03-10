ca6f4b1517fea6b91bbd1462d4771b376f0bb751
Austin Chick <auschick.work@gmail.com>
2025-03-09 18:47:33 -0700

    Update root page and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,34 @@
 import type { Metadata } from "next";
-import "@/styles/globals.css";
+import "@mantine/core/styles.css";
 import Analytics from "@/components/Analytics";
+import {
+  ColorSchemeScript,
+  createTheme,
+  mantineHtmlProps,
+  MantineProvider,
+} from "@mantine/core";
 
 export const metadata: Metadata = {
   title: "Chick-Bailas",
 };
+
+const theme = createTheme({
+  colors: {
+    "mint-green": [
+      "#e7fdee",
+      "#d6f6e1",
+      "#aeebc3",
+      "#84e0a2",
+      "#60d687",
+      "#49d075",
+      "#3bcd6b",
+      "#2bb55a",
+      "#20a14e",
+      "#098c40",
+    ],
+  },
+  primaryColor: "mint-green",
+});
 
 export default function RootLayout({
   children,
@@ -12,8 +36,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      <body>
+        <MantineProvider defaultColorScheme="dark" theme={theme}>
+          {children}
+        </MantineProvider>
+      </body>
       <Analytics />
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+import Contact from "@/components/Contact";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Services from "@/components/Services";
+import { Container, Space } from "@mantine/core";
+
 const Home = () => {
-  return <div />;
+  return (
+    <Container fluid>
+      <Header />
+      <Hero />
+      <Space h="md" />
+      <Services />
+      <Space h="md" />
+      <Contact />
+      <Space h="xl" />
+    </Container>
+  );
 };
 
 export default Home;
